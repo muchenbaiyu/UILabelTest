670f5c25e0f1c798f3a04e2b233c0ab661c17cc0
muchenbaiyu <muchenbaiyu@163.com>
2017-06-24 22:47:59 +0800

    Initial Commit

---
+++ UILabelTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILabelTest
+//
+//  Created by 闫纲忠 on 2017/6/24.
+//  Copyright © 2017年 ygz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UILabelTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILabelTest
+//
+//  Created by 闫纲忠 on 2017/6/24.
+//  Copyright © 2017年 ygz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
